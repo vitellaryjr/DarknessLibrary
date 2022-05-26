a29530f0c641a069b4ce815febf9b157ad4ef73a
vitellaryjr <43586575+vitellaryjr@users.noreply.github.com>
2022-05-26 15:48:04 -0700

    add testing stuff to mod, 'overlap' config option

--- libraries/DarknessLibrary/scripts/objects/DarknessOverlay.lua
+++ libraries/DarknessLibrary/scripts/objects/DarknessOverlay.lua
@@ -1,6 +1,6 @@
 local Darkness, super = Class(Object)
 
-function Darkness:init(alpha, color)
+function Darkness:init(alpha)
     super:init(self, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
     -- above everything, including ui, by default
     -- if you want it to be below ui, set its layer
@@ -11,27 +11,32 @@ function Darkness:init(alpha, color)
 
     self.alpha = alpha or 1
     self.style = Kristal.getLibConfig("darkness", "style")
+    self.overlap = Kristal.getLibConfig("darkness", "overlap")
 end
 
 function Darkness:draw()
     local canvas = Draw.pushCanvas(SCREEN_WIDTH, SCREEN_HEIGHT)
     love.graphics.setColor(1-self.alpha, 1-self.alpha, 1-self.alpha)
     love.graphics.rectangle("fill",0,0,SCREEN_WIDTH,SCREEN_HEIGHT)
-    love.graphics.setBlendMode("add")
+    if self.overlap then
+        love.graphics.setBlendMode("add")
+    else
+        love.graphics.setBlendMode("lighten", "premultiplied")
+    end
     for _,light in ipairs(Game.stage:getObjects(LightSource)) do
-        local x, y = light:getRelativePosition(0,0, self)
+        local x, y = light:getRelativePos(0,0, self)
         local color = Utils.copy(light.color)
         local alpha = color[4] or light.alpha
         local radius = light:getRadius()
 
-        if self.style = "solid" then
+        if self.style == "solid" then
             love.graphics.setColor(Utils.lerp({0,0,0}, color, alpha))
             love.graphics.circle("fill", x, y, radius)
-        elseif self.style = "soft" then
+        elseif self.style == "soft" then
             love.graphics.setColor(Utils.lerp({0,0,0}, color, alpha/2))
             love.graphics.circle("fill", x, y, radius)
             love.graphics.setColor(Utils.lerp({0,0,0}, color, alpha))
-            love.graphics.circle("fill", x, y, radius*0.75)
+            love.graphics.circle("fill", x, y, radius*2/3)
         end
     end
     love.graphics.setBlendMode("alpha")
--- mod.lua
+++ mod.lua
@@ -1,3 +1,11 @@
 function Mod:init()
-    print("Loaded "..self.info.name.."!")
+    Utils.hook(Map, "onEnter", function(orig, map)
+        orig(map)
+        Game.world:spawnObject(DarknessOverlay(1), WORLD_LAYERS["below_ui"])
+        for _,chara in ipairs(Game.stage:getObjects(Character)) do
+            local light = LightSource(chara.width/2, chara.height/2, 80)
+            light.alpha = 1
+            chara:addChild(light)
+        end
+    end)
 end
